08a798f43c8ffaf08ec37fc97b783b62beb5154d
linshenqi@outlook.com <linshenqi@outlook.com>
2018-03-04 18:07:14 +0800

    create

---
+++ dll_test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using net_dll;
+
+namespace dll_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello h = new Hello();
+            h.print();
+            
+        }
+    }
+}
---
+++ net_dll/hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net_dll
+{
+    public class Hello
+    {
+        public void print()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
